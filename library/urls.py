b4bc6aaa81e077bc731ea218dad998f1adb0904d
JackLindgren <jtlindgren88@gmail.com>
2021-07-19 21:10:00 -0700

    add form to create author

--- library/urls.py
+++ library/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('authors', views.AuthorListView.as_view(), name='authors'),
     path('authors/<int:author_id>', views.books_by_author, name='authors_works'),
-    path('books/<int:pk>', views.BookDetailView.as_view(), name='book_detail')
+    path('books/<int:pk>', views.BookDetailView.as_view(), name='book_detail'),
+    path('add_author', views.add_author, name='add_author')
 ]
--- library/views.py
+++ library/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 
-from .models import Author, Book
+from .models import Author, Book, Country
 
 
 class AuthorListView(generic.ListView):
@@ -31,3 +31,21 @@ def books_by_author(request, author_id):
         'books': books
     }
     return render(request, 'library/author_books.html', context)
+
+
+def add_author(request):
+    print(request.method)
+    if request.method == 'POST':
+        new_author = Author()
+        new_author.first_name = request.POST['first_name']
+        new_author.last_name = request.POST['last_name']
+        new_author.birth_date = request.POST['birth_date']
+        new_author.death_date = request.POST['death_date']
+        new_author.country = Country.objects.get(pk=request.POST['country'])
+        new_author.save()
+        print(f'created author: {new_author.first_name} {new_author.last_name}')
+        return HttpResponseRedirect(reverse('library:authors'))
+    else:
+        countries = Country.objects.all()
+        context = {'countries': countries}
+        return render(request, 'library/add_author.html', context)
